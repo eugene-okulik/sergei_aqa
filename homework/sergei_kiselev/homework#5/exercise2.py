4280a9634ee63b1485535b67045096ed56678476
Sergei Kiselev <sergei_qa@icloud.com>
2024-07-09 22:42:50 +0200

    completed homework 5

---
+++ homework/sergei_kiselev/homework#5/exercise2.py
@@ -0,0 +1,13 @@
+result1 = 'результат операции: 42'
+result2 = 'результат операции: 514'
+result3 = 'результат работы программы: 9'
+y = 10
+list1 = result1.split()[-1]
+list11 = int(list1) + y
+list2 = result2.split()[-1]
+list12 = int(list2) + y
+list3 = result3.split()[-1]
+list13 = int(list3) + y
+print(list11)
+print(list12)
+print(list13)
---
+++ homework/sergei_kiselev/homework#5/exercise3.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+
+my_text = f'Students {students} study these subjects: {subjects}'
+print(my_text)
