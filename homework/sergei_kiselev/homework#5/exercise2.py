c70a418f5ea9846a3525bf361cd322ee1fa43cf4
Sergei Kiselev <sergei_qa@icloud.com>
2024-07-13 11:36:55 +0200

    used 'slice' and 'index' to take a part of line

--- homework/sergei_kiselev/homework#5/exercise2.py
+++ homework/sergei_kiselev/homework#5/exercise2.py
@@ -1,13 +1,12 @@
 result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
-y = 10
 list1 = result1[-2:]
-list11 = int(list1) + y
+list11 = int(list1) + 10
 list2 = result2[-3:]
-list12 = int(list2) + y
+list12 = int(list2) + 10
 list3 = result3[-1:]
-list13 = int(list3) + y
+list13 = int(list3) + 10
 print(list11)
 print(list12)
 print(list13)
