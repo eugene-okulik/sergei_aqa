a1887b84d5d7c68befbaef0e6469f2d1d9a04a46
Sergei Kiselev <sergei_qa@icloud.com>
2024-07-13 11:30:54 +0200

    used 'slice' and 'index' to take a part of line

--- homework/sergei_kiselev/homework#5/exercise2.py
+++ homework/sergei_kiselev/homework#5/exercise2.py
@@ -2,11 +2,11 @@ result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 y = 10
-list1 = result1.split()[-1]
+list1 = result1[-2:]
 list11 = int(list1) + y
-list2 = result2.split()[-1]
+list2 = result2[-3:]
 list12 = int(list2) + y
-list3 = result3.split()[-1]
+list3 = result3[-1:]
 list13 = int(list3) + y
 print(list11)
 print(list12)
--- homework/sergei_kiselev/homework#5/exercise3.py
+++ homework/sergei_kiselev/homework#5/exercise3.py
@@ -5,3 +5,4 @@ subjects = ', '.join(subjects)
 
 my_text = f'Students {students} study these subjects: {subjects}'
 print(my_text)
+
