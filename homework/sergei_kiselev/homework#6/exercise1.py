5e37e1203f2cdf367639cef754cab3c42eb6f3e4
Sergei Kiselev <sergei_qa@icloud.com>
2024-08-05 23:00:40 +0200

    changed the indents if exrecise1

--- homework/sergei_kiselev/homework#6/exercise1.py
+++ homework/sergei_kiselev/homework#6/exercise1.py
@@ -6,15 +6,15 @@ part_of_word = 'ing'
 words = text.split()
 fin_words = []
 for word in words:
-        if word.endswith('.'):
-                new_word = word.replace('.', '')
-                fin_word = f'{new_word}{part_of_word}.'
-        elif word.endswith(','):
-                new_word = word.replace(',', '')
-                fin_word = f'{new_word}{part_of_word},'
-        else:
-                fin_word = f'{word}{part_of_word}'
+    if word.endswith('.'):
+        new_word = word.replace('.', '')
+        fin_word = f'{new_word}{part_of_word}.'
+    elif word.endswith(','):
+        new_word = word.replace(',', '')
+        fin_word = f'{new_word}{part_of_word},'
+    else:
+        fin_word = f'{word}{part_of_word}'
 
-        fin_words.append(fin_word)
+    fin_words.append(fin_word)
 
 print(' '.join(fin_words))
